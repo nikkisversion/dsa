48bb751363408719cb0016de995e3f60f9fb6d09
Nikita Rai <nikrc15@gmail.com>
2026-03-31 21:04:10 +0530

    stream of chars

--- leetcode/system_design/2_data_stream_processing/2_stream_of_characters/stream_of_characters.go
+++ leetcode/system_design/2_data_stream_processing/2_stream_of_characters/stream_of_characters.go
@@ -126,3 +126,31 @@ func (this *StreamChecker) Query(letter byte) bool {
 	return false
 
 }
+
+/*
+Notes
+
+-- Comments:
+1. We use a trie to optmimise on time
+2. The strings are inserted in the trie in
+reverse order and the input stream is formed
+in reverse order to account for suffix checking
+foremost
+3. We used recursion in one solution but that added a lot of
+overhead
+4. A key point was to not traverse the whole input stream
+everytime a new input arrives. Because for n inputs, it will
+require traversing an n length input stream and then n
+iterations to check. n square time complexity. So we use
+the MaxLen variable.
+
+-- Problem Link:
+https://leetcode.com/problems/stream-of-characters?envType=problem-list-v2&envId=ssd-ssd2-data-stream-processing
+
+
+-- References:
+1. https://medium.com/@itachisasuke/implementing-a-search-engine-in-golang-trie-data-structure-c45152ddda24
+2. https://gist.github.com/pjcalvo/59563a2024e5d4966b6c28afe6d8fdc2
+
+
+*/
