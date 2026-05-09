fcb46fcb3d28cfa97ed542251eb76406cb5a8457
Nikita Rai <nikrc15@gmail.com>
2026-05-09 17:24:17 +0530

    102: binary tree level order traversal (bfs)

---
+++ leetcode/topics/breadth_first_search/102_binary_tree_level_order_traversal_bfs/bfs.go
@@ -0,0 +1,77 @@
+package binarytreelevelordertraversalbfs
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+
+	if root == nil {
+		return [][]int{}
+	}
+
+	queue := []*TreeNode{root}
+	res := [][]int{}
+	level := 0
+
+	for true {
+
+		if len(queue) == 0 {
+			break
+		}
+
+		l := len(queue)
+		levelNodes := []int{}
+		for i := 0; i < l; i++ {
+
+			current := queue[0]
+			queue[0] = nil
+			queue = queue[1:]
+
+			if current == nil {
+				continue
+			}
+
+			levelNodes = append(levelNodes, current.Val)
+
+			if current.Left != nil {
+				queue = append(queue, current.Left)
+			}
+
+			if current.Right != nil {
+				queue = append(queue, current.Right)
+			}
+		}
+
+		res = append(res, levelNodes)
+		level++
+	}
+
+	return res
+
+}
+
+/*
+Notes
+
+Problem Link:
+https://leetcode.com/problems/binary-tree-level-order-traversal
+
+Comments:
+
+1. The number of elements in the queue is the number of elements at that level.
+
+2. We are setting queue[0] = nil to free up memory to be collected by the GC later.
+
+*/
